b07d677c9291521422ef90c4bba6605ef7cf4eea
Colin T.A. Gray <colinta@gmail.com>
2013-04-10 09:24:46 -0600

    ST3

--- change_quotes.py
+++ change_quotes.py
@@ -43,15 +43,7 @@ CHANGE_QUOTE = {
 
 class ChangeQuotesCommand(sublime_plugin.TextCommand):
     def run(self, edit, **kwargs):
-        regions = [region for region in self.view.sel()]
-
-        # any edits that are performed will happen in reverse; this makes it
-        # easy to keep region.a and region.b pointing to the correct locations
-        def get_end(region):
-            return region.end()
-        regions.sort(key=get_end, reverse=True)
-
-        for region in regions:
+        for region in self.view.sel():
             try:
                 error = self.run_each(edit, region, **kwargs)
             except Exception as exception:
